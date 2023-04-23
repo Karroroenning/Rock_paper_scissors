cb73f3ef4090969f5d9c02611c0eba640e15a430
karolina Rönning <Karroroenning@gmail.com>
2023-04-23 18:57:27 +0000

    added js script, result text to who wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ var yourScore = 0;
 var computer;
 var computerScore = 0;
 
-var choices = ['rock', 'paper', 'scissors'];
+const resultDisplay = document.getElementById('result')
+let result
+
+var choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+// Choose the choice img
 
 window.onload = function() {
     for (let i = 0; i < 5; i++) {
-        // <img id="rock" src=ROCK.png>
         let choice = document.createElement('img');
         choice.id = choices[i]
         choice.src = choices[i] + '.png';
@@ -21,33 +25,120 @@ window.onload = function() {
    
 
     // Random for the computer
-    computer = choices[Math.floor(Math.random() * 3)]; 
+    computer = choices[Math.floor(Math.random() * 5)]; 
     document.getElementById('computer-choice').src = computer + '.png';
 
     // check who wins
+
     if (user === computer) {
         yourScore += 1;
         computerScore += 1;
     }
+    
     else {
-         if (user === 'rock'){
-            if (computer === 'scissors') {
+        // user choose scissors
+         if (user === 'scissors') {
+            if (computer === 'paper') {
                 yourScore +=1;
+                result = 'You win! Scissors cuts paper'
             }
-            else if (computer === 'paper') {
+            else if (computer === 'rock') {
                 computerScore += 1;
+                result = 'You lost! Rock crushes scissors'
             }
-    }
-    else if (user === 'rock') {
-        if (computer === 'scissors') {
+            else if (computer === 'lizard') {
+                yourScore += 1;
+                result = 'You Win! Scissors decapitates lizard'
+            }
+            else if (computer === 'spock') {
+                computerScore += 1;
+                result = 'You lost! spock smashes scissors'
+            }
+        }
+    else 
+    // User choose rock
+        if (user === 'rock') {
+            if (computer === 'lizard') {
+            yourScore +=1;
+            result = 'You win! Rock cruches lizard'
+            }
+            else if (computer === 'spock') {
+            computerScore += 1;
+            result = 'You lost! Spock vaporizes rock'
+            }
+         else if (computer === 'scissors') {
             yourScore += 1;
+            result = 'You Win! Rock crushes scissors'
         }
         else if (computer === 'paper') {
             computerScore += 1;
+            result = 'You lost! Paper covers rock'
         }
     }
+    // user choose paper
+    if (user === 'paper') {
+        if (computer === 'scissors') {
+            computerScore +=1;
+            result = 'You lost! Scissors cuts paper'
+        }
+        else if (computer === 'rock') {
+            yourScore += 1;
+            result = 'You win! Paper covers rock'
+        }
+        else if (computer === 'lizard') {
+            computerScore += 1;
+            result = 'You lost! Lizard eats paper'
+        }
+        else if (computer === 'spock') {
+            yourScore += 1;
+            result = 'You Win! Paper disproves spock'
+        }
+    }
+else 
+// User choose lizard
+    if (user === 'lizard'){
+        if (computer === 'paper') {
+        yourScore +=1;
+        result = 'You win! Lizard eats paper'
+        }
+        else if (computer === 'rock') {
+        computerScore += 1;
+        result = 'You lost! Rock crushes lizard'
+        }
+     else if (computer === 'spock') {
+        yourScore += 1;
+        result = 'You Win! Lizard poisons spock'
+    }
+    else if (computer === 'scissors') {
+        computerScore += 1;
+        result = 'You lost! scissors decapitates lizard'
+    }
+}
+else 
+// User choose spock
+    if (user === 'spock'){
+        if (computer === 'scissors') {
+        yourScore +=1;
+        result = 'You win! Spock smashes scissors'
+        }
+        else if (computer === 'rock') {
+        yourScore += 1;
+        result = 'You win! Spock vaporizes rock'
+        }
+     else if (computer === 'lizard') {
+        computerScore += 1;
+        result = 'You lost! Lizard poisons spock'
+    }
+    else if (computer === 'paper') {
+        computerScore += 1;
+        result = 'You lost! paper disproves spock'
+    }
+}
+    
 }
 
 document.getElementById('your-score').innerHTML = yourScore;
 document.getElementById('computer-score').innerHTML = computerScore;
+
+resultDisplay.innerHTML = result
 }
